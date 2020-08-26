f0b1c1a22971c49059e68fa4a1ecb618af4c6d05
Zachary Bombay <zacharybombay@gmail.com>
2020-08-25 21:21:48 -0400

    adjust config org file

--- dot_emacs.d/init.el
+++ dot_emacs.d/init.el
@@ -5,11 +5,6 @@
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 (package-initialize)
 
-;; Ensure that use-package is installed.
-;;
-;; If use-package isn't already installed, it's extremely likely that this is a
-;; fresh installation! So we'll want to update the package repository and
-;; install use-package before loading the literate configuration.
 (when (not (package-installed-p 'use-package))
   (package-refresh-contents)
   (package-install 'use-package))
